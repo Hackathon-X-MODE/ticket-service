e331b77effa9f7e0f9287a77405c8b3ff9ef00f8
Alexander Chapchuk <BigTows@yandex.ru>
2023-05-21 13:47:56 +0300

    Fix mapping https://github.com/Hackathon-X-MODE/ticket-service/issues/2

--- src/main/java/com/example/example/mapper/OrderTicketMapper.java
+++ src/main/java/com/example/example/mapper/OrderTicketMapper.java
@@ -1,13 +1,19 @@
 package com.example.example.mapper;
 
 import com.example.example.configuration.MapperConfiguration;
+import com.example.example.domain.CommentAttachmentOrderTicket;
 import com.example.example.domain.OrderTicketEntity;
+import com.example.example.model.CommentAttachDto;
 import com.example.example.model.OrderTicketDto;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(config = MapperConfiguration.class)
 public interface OrderTicketMapper {
 
-
     OrderTicketDto toDto(OrderTicketEntity orderTicketEntity);
+
+
+    @Mapping(target = "id", source = "commentId")
+    CommentAttachDto toDto(CommentAttachmentOrderTicket commentAttachmentOrderTicket);
 }
