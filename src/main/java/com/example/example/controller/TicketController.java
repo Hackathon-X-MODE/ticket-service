287c3b92f4bec7214310c662b08a048c9a11ea89
Alexander Chapchuk <BigTows@yandex.ru>
2023-05-22 19:44:58 +0300

    Move notify to internal selection.

--- src/main/java/com/example/example/controller/TicketController.java
+++ src/main/java/com/example/example/controller/TicketController.java
@@ -2,31 +2,22 @@ package com.example.example.controller;
 
 import com.example.example.model.OrderTicketDto;
 import com.example.example.model.OrderTicketFilter;
-import com.example.example.model.comment.CommentDto;
-import com.example.example.service.TicketOrderReceiverService;
 import com.example.example.service.TicketOrderService;
-import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping
 @RequiredArgsConstructor
 public class TicketController {
 
-    private final TicketOrderReceiverService ticketOrderReceiverService;
-
     private final TicketOrderService ticketOrderService;
 
-    @Operation(summary = "Уведомить сервис об изменениях в комментарии", description = "Для переданного комментария будет создан")
-    @PostMapping("/notify")
-    @ResponseStatus(HttpStatus.ACCEPTED)
-    public void notify(@RequestBody CommentDto commentDto) {
-        this.ticketOrderReceiverService.notify(commentDto);
-    }
 
 
     @GetMapping
---
+++ src/main/java/com/example/example/controller/internal/TicketController.java
@@ -0,0 +1,26 @@
+package com.example.example.controller.internal;
+
+import com.example.example.model.comment.CommentDto;
+import com.example.example.service.TicketOrderReceiverService;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/private")
+@RequiredArgsConstructor
+public class TicketController {
+
+    private final TicketOrderReceiverService ticketOrderReceiverService;
+
+    @Operation(
+            summary = "Сервисный метод для уведомления сервиса об изменениях в комментарии",
+            description = "Для переданного комментария будет создан тикет, если тикет не подходит, будет удален."
+    )
+    @PostMapping("/notify")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public void notify(@RequestBody CommentDto commentDto) {
+        this.ticketOrderReceiverService.notify(commentDto);
+    }
+}
